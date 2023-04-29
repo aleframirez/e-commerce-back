ec04b8552b47bc0215b746e53c4ce1c08ae31d69
Alejandro <ale.970@hotmail.com>
2023-04-29 15:32:27 -0300

    A.R: Implementacion de Google Identity

--- controller/auth.js
+++ controller/auth.js
@@ -1,7 +1,8 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
-const { generateJwT } = require('../helpers/generate-jwt')
+const { generateJwT } = require("../helpers/generate-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
@@ -46,6 +47,52 @@ const login = async (req, res = response) => {
   }
 };
 
+const googleSignIn = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { name, email, img } = await googleVerify(id_token);
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      // Si no existe ningun usuario con ese correo, lo creamos.
+      const data = {
+        name,
+        email,
+        password: ":D",
+        img,
+        role: "USER_ROLE",
+        google: true,
+      };
+
+      user = new User(data);
+      await user.save();
+    }
+
+    // Si el usuario esta activo en la DB
+    if (!user.status) {
+      return res.status(401).json({
+        msg: "Talk to administrator, user blocked",
+      });
+    }
+
+    // Generamos el JwT
+    const token = await generateJwT(user.id);
+
+    res.json({
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      ok: false,
+      msg: "The token could not be verified",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignIn,
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,24 @@
-const { Router } = require('express');
-const { check } = require('express-validator');
-const { login } = require('../controller/auth');
+const { Router } = require("express");
+const { check } = require("express-validator");
+const { login, googleSignIn } = require("../controller/auth");
+const { validateFields } = require("../middlewares");
 
 const router = Router();
 
-router.post('/login', login)
+router.post(
+  "/login",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required").not().isEmpty(),
+    validateFields,
+  ],
+  login
+);
+
+router.post(
+  "/google",
+  [check("id_token", "id_token is required").not().isEmpty(), validateFields],
+  googleSignIn
+);
 
 module.exports = router;
